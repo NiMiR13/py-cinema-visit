693ec6b90f608adcd7bbfb0acae755e787e47c10
NiMiR13 <r0932665555@gmail.com>
2026-02-01 12:57:30 +0200

    Final PEP8 fixes and cleanup

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+    customers: list,
+    hall_number: int,
+    cleaner: str,
+    movie: str
+) -> None:
+    customer_instances = []
+    for customer_data in customers:
+        new_customer = Customer(
+            customer_data["name"],
+            customer_data["food"]
+        )
+        customer_instances.append(new_customer)
+        CinemaBar.sell_product(new_customer.food, new_customer)
+
+    staff_cleaner = Cleaner(cleaner)
+    hall = CinemaHall(hall_number)
+
+    hall.movie_session(movie, customer_instances, staff_cleaner)
---
+++ app/people/cinema_staff.py
@@ -0,0 +1,6 @@
+class Cleaner:  # Проверь написание здесь!
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def clean_hall(self, hall_number: int) -> None:
+        print(f"Cleaner {self.name} is cleaning hall number {hall_number}.")
